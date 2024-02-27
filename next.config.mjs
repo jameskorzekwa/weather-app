60493555f91ad7691aad9bc051e71c531f0fcc70
James Korzekwa <james.korzekwa@gmail.com>
2024-02-26 19:58:12 -0700

    test webpage reload

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
     experimental: {
         missingSuspenseWithCSRBailout: false
     },
-    env: {
-        VERCEL_GIT_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA || 'local'
-    }
 };
 
 export default nextConfig;
