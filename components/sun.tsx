c77fb49e3bf3fb008d706c34053ec6fc15b3163f
James Korzekwa <james.korzekwa@gmail.com>
2024-03-02 10:35:40 -0700

    use open meteo

--- components/sun.tsx
+++ components/sun.tsx
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 
 const StyledSun = styled.g`
     will-change: transform;
-    animation: spin 150s infinite linear;
-
+    animation: spin 100s infinite linear;
     @keyframes spin {
         100% {
             transform: rotate(360deg);
@@ -12,17 +11,18 @@ const StyledSun = styled.g`
     }
 `;
 export default function Sun() {
+    const numberOfRays = 14;
     return (
         <StyledSun>
             <ellipse fill={'#F1C40F'} cx="0" cy="0" rx={'60'} ry={'60'} />
-            {Array.from(Array(14), (_e, i) => (
+            {Array.from(Array(numberOfRays), (_e, i) => (
                 <rect
                     key={i}
                     fill={'#F1C40F'}
                     width={10}
                     height={40}
                     rx={5}
-                    transform={`rotate(${(360 / 16) * i + 1}) translate(-5, -110)`}
+                    transform={`rotate(${(360 / numberOfRays) * i}) translate(-5, -110)`}
                 />
             ))}
         </StyledSun>
